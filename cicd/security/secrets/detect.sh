aed64299c78b0ea03ce5c5fc41ea386afb2ae300
Simon Kowallik <github@simonkowallik.com>
2020-02-23 16:22:26 +0100

    secrets/detect.sh now ignores secrets line numbers [skip ci]

--- cicd/security/secrets/detect.sh
+++ cicd/security/secrets/detect.sh
@@ -2,20 +2,21 @@
 
 # scan whole repository and compare to baseline
 _date=$(date +%s)
-detect-secrets scan | jq '.results | del(.|.["secrets.baseline.json"])' > ${_date}_secrets.scan.json
-diff secrets.baseline.json ${_date}_secrets.scan.json >/dev/null
+detect-secrets scan | jq '.results | del(.|.["secrets.baseline.json"]) | del(.[] | .[].line_number)' > ${_date}_secrets.scan.json
+cat secrets.baseline.json | jq '. | del(.[] | .[].line_number)' > noLineNumbers.secrets.baseline.json
+diff noLineNumbers.secrets.baseline.json ${_date}_secrets.scan.json >/dev/null
 if [[ $? -ne 0 ]]
 then
   echo -e "\e[91m* ALERT: Detected new secrets violating the secrets baseline!\e[39m"
   echo "  detected secrets:"
-  cat ${_date}_secrets.scan.json
+  diff -ruN noLineNumbers.secrets.baseline.json ${_date}_secrets.scan.json
   echo
   # exit 1 to abort/fail CI pipeline
-  rm -f ${_date}_secrets.scan.json
+  rm -f ${_date}_secrets.scan.json noLineNumbers.secrets.baseline.json
   [[ "$ENFORCE" == 0 ]] || exit 1
 else
   # file are the same
   echo -e "\e[92m* PASS: No violation of secrets baseline found.\e[39m"
 fi
 
-rm -f ${_date}_secrets.scan.json
+rm -f ${_date}_secrets.scan.json noLineNumbers.secrets.baseline.json
